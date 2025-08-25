a7d37fd387cd40886a9a8ff298c1949daa1f8882
Roofle Dev <developer@roofle.com>
2025-08-25 14:29:50 -0500

    add note

--- samples/map-drawing-terradraw/index.ts
+++ samples/map-drawing-terradraw/index.ts
@@ -341,6 +341,9 @@ loader.load().then(async () => {
 
         draw.on("finish", (id, type) => {
             console.log('finish', id, type)
+
+            // I can't replicate it here, but my application results in an empty snapshot for the duplicated events
+            console.log(draw.getSnapshot());
         });
 
         // [END maps_drawing_terradraw_modes]
